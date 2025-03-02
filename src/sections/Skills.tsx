d80bce7de35e642dcb07b61c93f03af1c8084f58
Axel Estrada <axele1524@gmail.com>
2025-03-01 20:16:27 -0600

    refactor render skills method to improve readability

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -1,43 +1,113 @@
 "use client";
 
-import { TailwindIcon } from "@/icons/TailwindIcon";
-import { ReactIcon } from "@/icons/ReactIcon";
-import { SassIcon } from "@/icons/SassIcon";
-import { SQLiteIcon } from "@/icons/SQLiteIcon";
-import { ReduxIcon } from "@/icons/ReduxIcon";
-import { SQLIcon } from "@/icons/SQLIcon";
-import { GitIcon } from "@/icons/GitIcon";
+import { useState } from "react";
 import { HugeiconsIcon, HugeiconsProps } from "@hugeicons/react";
 import {
-  Alert01Icon,
-  DatabaseIcon,
-  GitForkIcon,
   Home01Icon,
-  Layers01Icon,
   PaintBrush04Icon,
+  DatabaseIcon,
   SourceCodeIcon,
+  GitForkIcon,
+  Layers01Icon,
+  Alert01Icon,
 } from "@hugeicons/core-free-icons";
-import { useState } from "react";
-import { HtmlIcon } from "@/icons/HtmlIcon";
-import { NextjsIcon } from "@/icons/NextjsIcon";
-import { CssIcon } from "@/icons/CssIcon";
-import { JavascriptIcon } from "@/icons/JavascriptIcon";
-import { FirebaseIcon } from "@/icons/FirebaseIcon";
-import { TypescriptIcon } from "@/icons/TypescriptIcon";
-import { GithubIcon } from "@/icons/GithubIcon";
-import { GitlabIcon } from "@/icons/GitlabIcon";
-import { GithubCodespacesIcon } from "@/icons/GithubCodespacesIcon";
-import { BitBucketIcon } from "@/icons/BitBucketIcon";
-import { GithubActionsIcon } from "@/icons/GithubActionsIcon";
-import { VsCodeIcon } from "@/icons/VsCodeIcon";
-import { FigmaIcon } from "@/icons/FigmaIcon";
-import { BabelIcon } from "@/icons/BabelIcon";
-import { NpmIcon } from "@/icons/NpmIcon";
-import { DockerIcon } from "@/icons/DockerIcon";
-import { StackOverflowIcon } from "@/icons/StackOverflowIcon";
+import {
+  TailwindIcon,
+  ReactIcon,
+  SassIcon,
+  SQLiteIcon,
+  ReduxIcon,
+  SQLIcon,
+  GitIcon,
+  HtmlIcon,
+  NextjsIcon,
+  CssIcon,
+  JavascriptIcon,
+  FirebaseIcon,
+  TypescriptIcon,
+  GithubIcon,
+  GitlabIcon,
+  GithubCodespacesIcon,
+  BitBucketIcon,
+  GithubActionsIcon,
+  VsCodeIcon,
+  FigmaIcon,
+  BabelIcon,
+  NpmIcon,
+  DockerIcon,
+  StackOverflowIcon,
+  MongoDBIcon,
+} from "@/icons"; // Import icons from local directory
 
 export const Skills = () => {
   const [activeTab, setActiveTab] = useState("home");
+
+  // Configuración de bordes: "b" = abajo, "r" = derecha, "l" = izquierda, "t" = arriba
+  const cellBorders = [
+    "border-b-3",
+    "border-b-3 border-l-3",
+    "border-b-3 border-l-3",
+    "border-b-3 border-l-3",
+    "border-b-3",
+    "border-b-3 border-l-3 col-span-2",
+    "border-b-3 border-l-3",
+    "border-b-3",
+    "border-b-3 border-l-3 col-span-2",
+    "border-b-3 border-l-3",
+    "",
+    "border-l-3",
+    "border-l-3",
+    "border-l-3"
+  ];
+  // Mapeo de habilidades según la pestaña activa
+  const skillsMap: Record<string, { icon: React.FC; index: number }[]> = {
+    home: [
+      { icon: TailwindIcon, index: 4 },
+      { icon: ReactIcon, index: 5 },
+      { icon: SassIcon, index: 6 },
+      { icon: SQLIcon, index: 7 },
+      { icon: ReduxIcon, index: 8 },
+      { icon: GitIcon, index: 9 },
+    ],
+    frontend: [
+      { icon: TailwindIcon, index: 4 },
+      { icon: ReactIcon, index: 5 },
+      { icon: SassIcon, index: 6 },
+      { icon: HtmlIcon, index: 8 },
+      { icon: ReduxIcon, index: 9 },
+      { icon: NextjsIcon, index: 7 },
+      { icon: CssIcon, index: 11 },
+      { icon: JavascriptIcon, index: 12 },
+    ],
+    database: [
+      { icon: SQLIcon, index: 4 },
+      { icon: FirebaseIcon, index: 5 },
+      { icon: SQLiteIcon, index: 6 },
+      { icon: MongoDBIcon, index: 8 },
+    ],
+    languages: [
+      { icon: HtmlIcon, index: 4 },
+      { icon: JavascriptIcon, index: 5 },
+      { icon: CssIcon, index: 6 },
+      { icon: TypescriptIcon, index: 8 },
+    ],
+    "version-control": [
+      { icon: GitIcon, index: 4 },
+      { icon: GithubIcon, index: 5 },
+      { icon: GitlabIcon, index: 6 },
+      { icon: GithubCodespacesIcon, index: 7 },
+      { icon: BitBucketIcon, index: 8 },
+      { icon: GithubActionsIcon, index: 9 },
+    ],
+    tools: [
+      { icon: NpmIcon, index: 4 },
+      { icon: VsCodeIcon, index: 5 },
+      { icon: BabelIcon, index: 6 },
+      { icon: DockerIcon, index: 7 },
+      { icon: FigmaIcon, index: 8 },
+      { icon: StackOverflowIcon, index: 9 },
+    ],
+  };
 
   return (
     <div className="w-full min-h-svh flex flex-col justify-center px-3">
@@ -45,7 +115,6 @@ export const Skills = () => {
       <h2 className="text-2.5xl font-extrabold mb-2">
         Mis habilidades técnicas
       </h2>
-
       <p className="text-foreground-65 text-lg">
         Este es un resumen de mis conocimientos en el desarrollo frontend.
         Navega por el menú lateral derecho para acceder a los apartados sobre
@@ -60,251 +129,88 @@ export const Skills = () => {
       </p>
 
       <div className="my-12 flex-row flex items-center gap-3">
-        <div className={`grid grid-cols-4 max-w-96 h-max w-full`}>
-          <Cell className="border-b-3" />
-          <Cell className="border-b-3 border-r-3 border-l-3" />
-          <Cell className="border-b-3" />
-          <Cell className="border-b-3 border-l-3"></Cell>
-          {activeTab === "home" && (
-            <>
-              <Cell className="border-b-3">
-                <TailwindIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3 col-span-2">
-                <ReactIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3">
-                <SassIcon />
-              </Cell>
-              <Cell className="border-b-3">
-                <SQLIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3 col-span-2">
-                <ReduxIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3">
-                <GitIcon />
-              </Cell>
-              <Cell />
-              <Cell className="border-r-3 border-l-3" />
-              <Cell />
-            </>
-          )}
+        {/* Grid de habilidades */}
+        <div className="grid grid-cols-4 max-w-96 h-max w-full">
+          {cellBorders.map((borderKeys, index) => {
+            const skill = skillsMap[activeTab]?.find((s) => s.index === index);
 
-          {activeTab === "frontend" && (
-            <>
-              <Cell className="border-b-3">
-                <TailwindIcon />
+            return (
+              <Cell key={index} className={borderKeys}>
+                {skill?.icon && <skill.icon />}
               </Cell>
-              <Cell className="border-b-3 border-l-3 col-span-2">
-                <ReactIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3">
-                <SassIcon />
-              </Cell>
-              <Cell className="border-b-3">
-                <HtmlIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3 col-span-2">
-                <ReduxIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3">
-                <NextjsIcon />
-              </Cell>
-              <Cell />
-              <Cell className="border-r-3 border-l-3">
-                <CssIcon />
-              </Cell>
-              <Cell>
-                <JavascriptIcon />
-              </Cell>
-            </>
-          )}
-
-          {activeTab === "database" && (
-            <>
-              <Cell className="border-b-3">
-                <SQLIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3 col-span-2">
-                <FirebaseIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3">
-                <SQLiteIcon />
-              </Cell>
-              <Cell />
-              <Cell className="border-r-3 border-l-3"></Cell>
-              <Cell></Cell>
-            </>
-          )}
-
-          {activeTab === "languages" && (
-            <>
-              <Cell className="border-b-3"></Cell>
-              <Cell className="border-b-3 border-l-3 col-span-2">
-                <JavascriptIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3"></Cell>
-              <Cell className="border-b-3"></Cell>
-              <Cell className="border-b-3 border-l-3 col-span-2">
-                <TypescriptIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3"></Cell>
-              <Cell />
-              <Cell className="border-r-3 border-l-3"></Cell>
-              <Cell></Cell>
-            </>
-          )}
-
-          {activeTab === "version-control" && (
-            <>
-              <Cell className="border-b-3">
-                <GitIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3 col-span-2">
-                <GithubIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3">
-                <GitlabIcon />
-              </Cell>
-              <Cell className="border-b-3">
-                <GithubCodespacesIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3 col-span-2">
-                <BitBucketIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3">
-                <GithubActionsIcon />
-              </Cell>
-              <Cell />
-              <Cell className="border-r-3 border-l-3"></Cell>
-              <Cell></Cell>
-            </>
-          )}
-
-          {activeTab === "tools" && (
-            <>
-              <Cell className="border-b-3">
-                <NpmIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3 col-span-2">
-                <VsCodeIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3">
-                <BabelIcon />
-              </Cell>
-              <Cell className="border-b-3">
-                <DockerIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3 col-span-2">
-                <FigmaIcon />
-              </Cell>
-              <Cell className="border-b-3 border-l-3">
-                <StackOverflowIcon />
-              </Cell>
-              <Cell />
-              <Cell className="border-r-3 border-l-3"></Cell>
-              <Cell></Cell>
-            </>
-          )}
-
-          <Cell className="border-l-3" />
+            );
+          })}
         </div>
 
-        <Menu activeTab={activeTab} handleClick={(tab) => setActiveTab(tab)} />
+        {/* Menú de navegación */}
+        <Menu activeTab={activeTab} handleClick={setActiveTab} />
       </div>
     </div>
   );
 };
 
-interface CellProps {
+const Cell = ({
+  className,
+  children,
+}: {
   className?: string;
   children?: React.ReactNode;
-}
+}) => (
+  <div
+    className={`border-accent h-20 w-full flex justify-center items-center p-3 ${className}`}
+  >
+    {children}
+  </div>
+);
 
-const Cell = ({ className, children }: CellProps) => {
-  return (
-    <div
-      className={`border-accent h-20 w-full justify-center items-center flex ${className} p-3`}
-    >
-      {children}
-    </div>
-  );
-};
-
-interface MenuProps {
+const Menu = ({
+  activeTab,
+  handleClick,
+}: {
   activeTab: string;
   handleClick: (tab: string) => void;
-}
+}) => {
+  const menuItems = [
+    { icon: Home01Icon, href: "home" },
+    { icon: PaintBrush04Icon, href: "frontend" },
+    { icon: DatabaseIcon, href: "database" },
+    { icon: SourceCodeIcon, href: "languages" },
+    { icon: GitForkIcon, href: "version-control" },
+    { icon: Layers01Icon, href: "tools" },
+  ];
 
-const Menu = ({ activeTab, handleClick }: MenuProps) => {
   return (
     <div className="bg-black dark:bg-white dark:bg-opacity-5 bg-opacity-10 p-1.5 rounded-full flex flex-col justify-center items-center">
-      <MenuItem
-        icon={Home01Icon}
-        href="home"
-        onClick={handleClick}
-        activeTab={activeTab}
-      />
-
-      <MenuItem
-        icon={PaintBrush04Icon}
-        href="frontend"
-        onClick={handleClick}
-        activeTab={activeTab}
-      />
-
-      <MenuItem
-        icon={DatabaseIcon}
-        href="database"
-        onClick={handleClick}
-        activeTab={activeTab}
-      />
-
-      <MenuItem
-        icon={SourceCodeIcon}
-        href="languages"
-        onClick={handleClick}
-        activeTab={activeTab}
-      />
-
-      <MenuItem
-        icon={GitForkIcon}
-        href="version-control"
-        onClick={handleClick}
-        activeTab={activeTab}
-      />
-
-      <MenuItem
-        icon={Layers01Icon}
-        href="tools"
-        onClick={handleClick}
-        activeTab={activeTab}
-      />
+      {menuItems.map(({ icon, href }) => (
+        <MenuItem
+          key={href}
+          icon={icon}
+          href={href}
+          onClick={handleClick}
+          activeTab={activeTab}
+        />
+      ))}
     </div>
   );
 };
-
-interface MenuItemProps {
-  icon: HugeiconsProps["icon"];
-  activeTab: string;
-  href: string;
-  onClick: (tab: string) => void;
-}
 
 const MenuItem = ({
   icon = Alert01Icon,
   activeTab,
   href,
   onClick,
-}: MenuItemProps) => {
-  const active = href === activeTab;
-
+}: {
+  icon: HugeiconsProps["icon"];
+  activeTab: string;
+  href: string;
+  onClick: (tab: string) => void;
+}) => {
+  const isActive = href === activeTab;
   return (
     <button
       onClick={() => onClick(href)}
       className={`my-2 bg-black dark:bg-white rounded-[10px] p-1 hover:bg-opacity-20 transition-colors ${
-        active
+        isActive
           ? "bg-opacity-20 dark:bg-opacity-15"
           : "bg-opacity-0 dark:bg-opacity-0"
       }`}
